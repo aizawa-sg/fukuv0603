a76cd2778d5ec13d4a6d44addf0e967d5a98d367
A601_01 <tnk.dat.class@gmail.com>
2021-06-03 09:36:39 +0900

    追加課題

--- fukuv0603/Form1.cs
+++ fukuv0603/Form1.cs
@@ -27,5 +27,25 @@ namespace fukuv0603
             label1.Left += int.Parse(textBox1.Text);
             label1.Top += int.Parse(textBox2.Text);
         }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            label1.Top = label1.Top - 30;
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            label1.Left = label1.Left + 30;
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            label1.Left = label1.Left - 30;
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            label1.Top = label1.Top + 30;
+        }
     }
 }
